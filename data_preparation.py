0a3caec81e4c9dff838a003da9ce3e99551219b9
Mia <44412313+mia-mia-w@users.noreply.github.com>
2021-06-21 12:44:59 -0400

    Update data_preparation.py

--- data_preparation.py
+++ data_preparation.py
@@ -1,28 +1,6 @@
 import os
 from pydub import AudioSegment
 
-def convert1(path):
-    # Change working directory
-    os.chdir(path)
-    audio_files = os.listdir()
-
-    for file in audio_files:
-        # spliting the file into the name and the extension
-        name, ext = os.path.splitext(file)
-        if ext == ".ogg":
-            ogg_sound = AudioSegment.from_ogg(file)
-            # rename them using the old name + ".wav"
-            ogg_sound.export("{0}.wav".format(name), format="wav")
-
-def delete_ogg(path):
-    dir_name = path
-    test = os.listdir(dir_name)
-
-    for item in test:
-        if item.endswith(".ogg"):
-            os.remove(os.path.join(dir_name, item))
-
-
 def convert_ogg(path):
     # os.chdir(path)
     # audio_files = os.listdir()
