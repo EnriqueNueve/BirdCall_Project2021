19cf5af380c35068b5b09052d0dccf31efd91a65
Mia <44412313+mia-mia-w@users.noreply.github.com>
2021-06-22 09:14:11 -0400

    Update Main.py

--- MFCC&FilterBanks/Main.py
+++ MFCC&FilterBanks/Main.py
@@ -45,47 +45,8 @@ def main():
     # feature_type: 'mfcc' or 'filter_banks'
     data, target, filenames = get_dataset_from_wavfile('wavfiles/realuav/', 'labels_new.csv', 1.5, 'mfcc', 'class2')
     X_test, X_train, y_test, y_train, train_index, test_index = get_train_test(data, target)
-    # X_test_processed, X_train_processed = pre_process(X_test, X_train)
-    # X_train_loaded_unloaded, y_train_loaded_unloaded, X_train_uav_noise, y_train_uav_noise = split_uav_criteria(data, target, train_index)
-    # X_test_loaded_unloaded, y_test_loaded_unloaded, X_test_uav_noise, y_test_uav_noise = split_uav_criteria(data, target, test_index)
-    # print("Report UAV vs Noise")
-    # clf_uav_noise = get_trained_classifier(X_train_uav_noise, y_train_uav_noise, X_test_uav_noise, y_test_uav_noise,
-    #                                        "gnb")
-    # predictions = np.array(clf_uav_noise.get_predictions(X_test_processed), dtype="object")
-    # print("Report Loaded vs Unloaded")
-    # clf_loaded_unloaded = get_trained_classifier(X_train_loaded_unloaded, y_train_loaded_unloaded,
-    #                                              X_test_loaded_unloaded, y_test_loaded_unloaded, "gnb")
-    #
-    # for index in range(predictions.size):
-    #     if predictions[index] == 'UAV':
-    #         pred_loaded_unloaded = clf_loaded_unloaded.get_predictions(X_test_processed[index].reshape(1, -1))
-    #         predictions[index] = pred_loaded_unloaded[0]
-    #
-    # print("Final Report in Sequence")
-    # print(classification_report(y_test, predictions))
-
     print("Final Report All Together")
     get_trained_classifier(X_train, y_train, X_test, y_test, "gnb")
-
-
-def load_json():
-    addr = "https://www.xeno-canto.org/api/2/recordings?query=troglodytes+troglodytes"
-    URLs = []
-    ids = []
-    files = []
-    for i in range(1, 1000):
-        URLs.append(addr + "&page=" + str(i))
-
-    for url in URLs:
-        results = requests.get(url)
-        print(results.status_code)
-        if results.status_code != 200:
-            break
-        data = results.json()
-        for r in data["recordings"]:
-            files.append(r["file-name"])
-            ids.append(r["id"])
-    print(len(files), len(ids))
 
 
 if __name__ == '__main__':
