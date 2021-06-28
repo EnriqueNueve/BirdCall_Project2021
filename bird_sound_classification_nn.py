59346058682bf03ce9c551f09162e615392acb56
Mia <44412313+mia-mia-w@users.noreply.github.com>
2021-06-28 09:58:20 -0400

    Update and rename bird_sound_classification_cnn.py to bird_sound_classification_nn.py

--- bird_sound_classification_nn.py
+++ bird_sound_classification_nn.py
@@ -1,3 +1,4 @@
+
 import IPython.display as ipd
 import numpy as np
 import pandas as pd
@@ -19,6 +20,7 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.callbacks import ModelCheckpoint
 import np_utils
+from sklearn.model_selection import train_test_split
 
 #### Dependencies ####
 
@@ -27,18 +29,26 @@ def extract_features(file_name):
         audio, sample_rate = librosa.load(file_name, res_type='kaiser_fast')
         mfccs = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
         mfccsscaled = np.mean(mfccs.T, axis=0)
-
     except Exception as e:
         print("Error encountered while parsing file: ", file_name)
         return None
     return mfccsscaled
+
+def features_fourier_tempogram(file_name):
+    audio, sample_rate = librosa.load(file_name, res_type='kaiser_fast')
+    hop_length = 512
+    oenv = librosa.onset.onset_strength(y=audio, sr=sample_rate, hop_length=hop_length)
+    fourier_tempograms = librosa.feature.fourier_tempogram(onset_envelope=oenv, sr=sample_rate,
+                                                           hop_length=hop_length)
+    fourier_tempogram = np.mean(fourier_tempograms.T, axis=0)
+
+    return fourier_tempogram
 
 def extract_features_chroma(file_name):
     try:
         audio, sample_rate = librosa.load(file_name, res_type='kaiser_fast')
         chroma_l = librosa.feature.chroma_stft(y=audio, sr=sample_rate)
         chromascaled = np.mean(chroma_l.T, axis=0)
-
     except Exception as e:
         print("Error encountered while parsing file: ", file_name)
         return None
@@ -49,7 +59,6 @@ def extract_features_mel(file_name):
         audio, sample_rate = librosa.load(file_name, res_type='kaiser_fast')
         mel_l = librosa.feature.melspectrogram(audio, sr=sample_rate)
         mel = np.mean(mel_l.T, axis=0)
-
     except Exception as e:
         print("Error encountered while parsing file: ", file_name)
         return None
@@ -57,20 +66,17 @@ def extract_features_mel(file_name):
 
 # Set the path to the full UrbanSound dataset
 fulldatasetpath = 'test_audio'
-
 metadata = pd.read_csv('data/test.csv')
-
 features = []
-
 for index, row in metadata.iterrows():
-    file_name = os.path.join(os.path.abspath(fulldatasetpath), str(row["primary_label"]) + '/',
-                             str(row["filename"]))
+    file_name = os.path.join(os.path.abspath(fulldatasetpath), str(row['primary_label']) + '/',
+                             str(row['filename']))
     # file_name = os.path.join(os.path.abspath(fulldatasetpath), 'fold' + str(row["fold"]) + '/',
     #                          str(row["file_name"]))
-
     class_label = row["primary_label"]
-    data = extract_features_chroma(file_name)
+    data = extract_features(file_name)
     features.append([data, class_label])
+    print('parsing' + file_name + '.' )
 
 # Convert into a Panda dataframe
 featuresdf = pd.DataFrame(features, columns=['feature', 'class_label'])
@@ -84,39 +90,28 @@ le = LabelEncoder()
 yy = to_categorical(le.fit_transform(y))
 
 # split the dataset
-from sklearn.model_selection import train_test_split
-
 x_train, x_test, y_train, y_test = train_test_split(X, yy, test_size=0.2, random_state=42)
-
 num_labels = yy.shape[1]
 filter_size = 2
 
 # Construct model
 model = Sequential()
-
-model.add(Dense(256, input_shape=(12,)))
+model.add(Dense(256, input_shape=(193,)))
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
-
 model.add(Dense(256))
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
-
 model.add(Dense(num_labels))
 model.add(Activation('softmax'))
 
 # Compile the model
 model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
-
 num_epochs = 100
 num_batch_size = 32
-
 start = datetime.now()
-
 model.fit(x_train, y_train, batch_size=num_batch_size, epochs=num_epochs, validation_data=(x_test, y_test), verbose=1)
-
 duration = datetime.now() - start
-
 print("Training completed in time: ", duration)
 
 score = model.evaluate(x_train, y_train, verbose=0)
